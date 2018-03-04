30c9c6e02ad9dc7c9fcecf987c3c0c6e79c3aaa7
Jae <lin6224@hotmail.com>
2018-03-04 18:18:08 -0500

    just add

--- test.go
+++ test.go
@@ -6,9 +6,17 @@ import "fmt"
 
 func main() {
    fmt.Println("Hello, World!")
+
+   var a, b  int
+   a = 10
+   var c int = 10
+   d := 20
+   fmt.Println( a, b, c, d )
 }
 
 
 // go command:
 //              go version
-//              go run test.go   -> run go
+//              go run test.go   -> run go application
+//              go build test.go  -> build direct application in OS
+
