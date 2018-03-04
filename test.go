7fe98afd247749615bb87d14da4c245c2b070198
Jae <lin6224@hotmail.com>
2018-03-04 17:26:36 -0500

    first commit

---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+// fmt package (I/O package) has Println method, we should be add this package
+import "fmt"
+
+func main() {
+   fmt.Println("Hello, World!")
+}
+
+
+// go command:
+//              go version
+//              go run test.go   -> run go
